24c65bd497bf127d412eabd1643c3e4db456ad47
pavankalyanarts <pavanteeda10@gmail.com>
2024-11-26 10:54:57 +0530

    built the cart popup for the mobile view(changes are currently done for mobile only)

--- commerce.js
+++ commerce.js
@@ -1,1 +1,45 @@
-console.log("hello world")
+let gloablCount = 0;
+
+
+const productIncrement = document.getElementById("productIncrementBtn");
+const productDecrement = document.getElementById("productDecrementBtn");
+
+const addProcductToCart = document.getElementById("addProcductToCart");
+const cartQuantity = document.getElementById("cartCount");
+cartQuantity.style.display = "none"
+
+productIncrement.addEventListener("click", increaseCount);
+
+function increaseCount(event){
+    gloablCount++
+    document.getElementById("productQuantity").innerHTML = gloablCount
+    console.log(gloablCount)
+}
+
+productDecrement.addEventListener("click", decraseCount);
+
+function decraseCount(event){
+
+    if(gloablCount <= 0){
+        gloablCount = 0
+        document.getElementById("productQuantity").innerHTML = gloablCount
+    }else{
+        gloablCount--
+        document.getElementById("productQuantity").innerHTML = gloablCount
+    }
+    
+    console.log(gloablCount)
+}
+
+
+addProcductToCart.addEventListener("click", addingProductToCart);
+
+function addingProductToCart(){
+    if(gloablCount > 0){
+        cartQuantity.style.display = "inline"
+        cartQuantity.innerHTML = gloablCount
+        
+    }else{
+        cartQuantity.style.display = "none"
+    }
+}
