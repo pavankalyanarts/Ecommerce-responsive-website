97b0fbcea235a82a84655fea7b502ff04ebb8054
pavankalyanarts <pavanteeda10@gmail.com>
2024-11-27 11:00:22 +0530

    added the cart functionalities

--- commerce.js
+++ commerce.js
@@ -1,45 +1,75 @@
-let gloablCount = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  let globalCount = 0;
 
+  const productIncrement = document.getElementById("productIncrementBtn");
+  const productDecrement = document.getElementById("productDecrementBtn");
 
-const productIncrement = document.getElementById("productIncrementBtn");
-const productDecrement = document.getElementById("productDecrementBtn");
+  const addProcductToCart = document.getElementById("addProcductToCart");
+  const cartQuantity = document.getElementById("cartCount");
+  cartQuantity.style.display = "none";
 
-const addProcductToCart = document.getElementById("addProcductToCart");
-const cartQuantity = document.getElementById("cartCount");
-cartQuantity.style.display = "none"
+  productIncrement.addEventListener("click", increaseCount);
 
-productIncrement.addEventListener("click", increaseCount);
+  function increaseCount(event) {
+    globalCount++;
+    document.getElementById("productQuantity").innerHTML = globalCount;
+    console.log(globalCount);
+  }
 
-function increaseCount(event){
-    gloablCount++
-    document.getElementById("productQuantity").innerHTML = gloablCount
-    console.log(gloablCount)
-}
+  productDecrement.addEventListener("click", decraseCount);
 
-productDecrement.addEventListener("click", decraseCount);
-
-function decraseCount(event){
-
-    if(gloablCount <= 0){
-        gloablCount = 0
-        document.getElementById("productQuantity").innerHTML = gloablCount
-    }else{
-        gloablCount--
-        document.getElementById("productQuantity").innerHTML = gloablCount
+  function decraseCount(event) {
+    if (globalCount <= 0) {
+      globalCount = 0;
+      document.getElementById("productQuantity").innerHTML = globalCount;
+    } else {
+      globalCount--;
+      document.getElementById("productQuantity").innerHTML = globalCount;
     }
+
+    console.log(globalCount);
+  }
+
+  addProcductToCart.addEventListener("click", addingProductToCart);
+
+  function addingProductToCart() {
+    if (globalCount > 0) {
+      cartQuantity.style.display = "inline";
+      cartQuantity.innerHTML = globalCount;
+    } else {
+      cartQuantity.innerHTML = ""  
+      cartQuantity.style.display = "none";
+    }
+  }
+
+  const viewCartItemsBtn = document.getElementById("viewCartItemsBtn");
+
+  viewCartItemsBtn.addEventListener("click", function () {
+    const productCartView = document.getElementById("productCartView");
+    const cartProductDetails = document.getElementById("cartProductDetails");
+    const productXquantity = document.getElementById("productXquantity");
+
+  productCartView.classList.toggle("view-cart");
+
+    if (globalCount > 0) {
+        cartProductDetails.style.display = "inline-block";
+        productXquantity.innerHTML = `$125.00 x ${globalCount}`;
+        document.getElementById("cartProductTotalPrice").innerHTML = `$${125 * globalCount}.00`
     
-    console.log(gloablCount)
-}
-
-
-addProcductToCart.addEventListener("click", addingProductToCart);
-
-function addingProductToCart(){
-    if(gloablCount > 0){
-        cartQuantity.style.display = "inline"
-        cartQuantity.innerHTML = gloablCount
-        
-    }else{
-        cartQuantity.style.display = "none"
+    } else {
+      cartProductDetails.style.display = "none";
     }
-}
+  });
+
+  const cartItemsRemovebtn = document.getElementById("cartItemsRemovebtn");
+
+  cartItemsRemovebtn.addEventListener("click", function(){
+    globalCount = 0;
+
+    document.getElementById("productQuantity").innerHTML = globalCount;
+    cartQuantity.innerHTML = globalCount;
+    document.getElementById("cartProductDetails").style.display = "none"    
+  })
+});
+
+
